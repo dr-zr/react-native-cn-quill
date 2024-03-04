f858dd9d61f8a9d001ea372ba90c5742312cdd77
zirlman <dragec.96@live.com>
2024-03-04 09:37:55 +0100

    style(editor): fix style for unordered list

--- src/constants/editor/quill_bubble_css.ts
+++ src/constants/editor/quill_bubble_css.ts
@@ -73,22 +73,22 @@ export const quill_bubble_css = (cdn: boolean) =>
    .ql-editor ul > li {
      list-style-type: none;
    }
-   
-   .ql-editor ol li.ql-indent-1:before,
-   .ql-editor ol li.ql-indent-4:before,
-   .ql-editor ol li.ql-indent-7:before {
+  
+   .ql-editor ul li.ql-indent-1:before,
+   .ql-editor ul li.ql-indent-4:before,
+   .ql-editor ul li.ql-indent-7:before {
         content: '\\2022'; /* disc */
     }
 
-    .ql-editor ol li.ql-indent-2:before,
-    .ql-editor ol li.ql-indent-5:before,
-    .ql-editor ol li.ql-indent-8:before {
+    .ql-editor ul li.ql-indent-2:before,
+    .ql-editor ul li.ql-indent-5:before,
+    .ql-editor ul li.ql-indent-8:before {
           content: '\\25E6'; /* circle */
     }
 
-    .ql-editor ol li.ql-indent-3:before,
-    .ql-editor ol li.ql-indent-6:before,
-    .ql-editor ol li.ql-indent-9:before {
+    .ql-editor ul li.ql-indent-3:before,
+    .ql-editor ul li.ql-indent-6:before,
+    .ql-editor ul li.ql-indent-9:before {
           content: '\\25A0'; /* square */
     }
 
