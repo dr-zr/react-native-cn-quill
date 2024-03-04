b832e245d3e64263fc6471c12f9d97a05cef06cb
zirlman <dragec.96@live.com>
2024-03-04 09:54:25 +0100

    style(editor): update ul content

--- src/constants/editor/quill_bubble_css.ts
+++ src/constants/editor/quill_bubble_css.ts
@@ -74,22 +74,26 @@ export const quill_bubble_css = (cdn: boolean) =>
      list-style-type: none;
    }
   
+   .ql-editor ul > li::before {
+      content: '\\2022'; /* disc */
+   }
+   
    .ql-editor ul li.ql-indent-1:before,
    .ql-editor ul li.ql-indent-4:before,
    .ql-editor ul li.ql-indent-7:before {
-        content: '\\2022'; /* disc */
+      content: '\\25E6'; /* circle */
     }
 
     .ql-editor ul li.ql-indent-2:before,
     .ql-editor ul li.ql-indent-5:before,
     .ql-editor ul li.ql-indent-8:before {
-          content: '\\25E6'; /* circle */
+      content: '\\25A0'; /* square */
     }
 
     .ql-editor ul li.ql-indent-3:before,
     .ql-editor ul li.ql-indent-6:before,
     .ql-editor ul li.ql-indent-9:before {
-          content: '\\25A0'; /* square */
+        content: '\\2022'; /* disc */
     }
 
    .ql-editor ul[data-checked=true],
