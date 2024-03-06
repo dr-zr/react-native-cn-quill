e839cb7d571338c72db0bf23ec4b259a740e5738
zirlman <dragec.96@live.com>
2024-03-06 13:23:14 +0100

    fix

--- src/constants/editor/editor_js.ts
+++ src/constants/editor/editor_js.ts
@@ -181,18 +181,18 @@ export const editor_js = `
       index: quill.getIndex(leaf),
       attributes: getAttributes(leaf.parent.domNode),
       tag: leaf.parent.domNode.tagName,
-      parent: leaf.parent.parent ? {
-        tag: leaf.parent.parent.domNode.tagName,
-        attributes: getAttributes(leaf.parent.parent.domNode),
-      } : null,
-      prev: leaf.parent.prev ? {
-        tag: leaf.parent.prev.domNode.tagName,
-        attributes: getAttributes(leaf.parent.prev.domNode),
-      } : null,
-      next: leaf.parent.next ? {
-        tag: leaf.parent.next.domNode.tagName,
-        attributes: getAttributes(leaf.parent.next.domNode),
-      } : null,
+      // parent: leaf.parent.parent ? {
+      //   tag: leaf.parent.parent.domNode.tagName,
+      //   attributes: getAttributes(leaf.parent.parent.domNode),
+      // } : null,
+      // prev: leaf.parent.prev ? {
+      //   tag: leaf.parent.prev.domNode.tagName,
+      //   attributes: getAttributes(leaf.parent.prev.domNode),
+      // } : null,
+      // next: leaf.parent.next ? {
+      //   tag: leaf.parent.next.domNode.tagName,
+      //   attributes: getAttributes(leaf.parent.next.domNode),
+      // } : null,
     } : {};
     const getLineJson = JSON.stringify({
       type: 'get-line',
